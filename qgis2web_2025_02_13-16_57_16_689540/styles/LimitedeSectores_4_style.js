36b0ffee94ab26b92d39588261d1bb50b041b403
oscar-data-mac <ohernandezm@cdmx.gob.mx>
2025-02-13 16:58:38 -0600

    Add files via upload

---
+++ qgis2web_2025_02_13-16_57_16_689540/styles/LimitedeSectores_4_style.js
@@ -0,0 +1,30 @@
+var size = 0;
+var placement = 'point';
+
+var style_LimitedeSectores_4 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; var value = feature.get("");
+    var labelFont = "20.8px \'Cabin\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "#ffffff";
+    var bufferWidth = 0.40000000000000013;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("Nombre_Sec") !== null) {
+        labelText = String(feature.get("Nombre_Sec"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(158,31,72,0.5725490196078431)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.748}),fill: new ol.style.Fill({color: 'rgba(213,180,60,0.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
